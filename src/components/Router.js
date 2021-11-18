4e1990020876eca51b6572e42c77450939d8be98
honghyeong <scarletgold0601@gmail.com>
2021-11-18 23:19:53 +0900

    fix: 챕터1 리액트라우터 버전 변경

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "../routes/Home";
 import Auth from "../routes/Auth";
 
-const AppRouter = () => {
-  //   const [isLoggedIn, setIsLoggedIn] = useState(true);
-  const isLoggedIn = true;
+const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
-      <Routes>
+      <Switch>
         {isLoggedIn ? (
           <>
-            <Route exact path="/" component={Home} />
+            <Route>
+              <Home />
+            </Route>
           </>
         ) : (
-          <Route exact path="/" component={Auth} />
+          <Route>
+            <Auth />
+          </Route>
         )}
-      </Routes>
+      </Switch>
     </Router>
   );
 };
