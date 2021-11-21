b4df5d7af67d517b15140682c10766e8bf3be77b
honghyeong <scarletgold0601@gmail.com>
2021-11-21 12:53:49 +0900

    chatper3: firestore 사용

--- src/fBase.js
+++ src/fBase.js
@@ -4,6 +4,7 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -19,3 +20,4 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 export const authService = getAuth();
+export const dbService = getFirestore();
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,44 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { dbService } from "../fBase";
+import { addDoc, collection } from "firebase/firestore";
 const Home = () => {
-  return <span>Home</span>;
+  const [tweet, setTweet] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const docRef = await addDoc(collection(dbService, "tweets"), {
+        tweet,
+        createAt: Date.now(),
+      });
+      console.log("Document Written", docRef);
+    } catch (e) {
+      console.log("Error adding document: ", e);
+    }
+
+    setTweet("");
+  };
+
+  const onChange = (event) => {
+    const { value } = event.target;
+    setTweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="What is on your mind?"
+          value={tweet}
+          maxLength={120}
+          onChange={onChange}
+        />
+        <input type="submit" value="Tweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
